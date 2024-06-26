6cb256dcc7dc11994b036030300c037f83f4f4e1
Bongjun <bongjun.jang@kaist.ac.kr>
2024-06-26 23:38:39 +0900

    update setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 ln -sf $(pwd)/config.fish ~/.config/fish/config.fish
 ln -sf $(pwd)/.tmux.conf ~/.tmux.conf
 ln -sf $(pwd)/.tmux.conf.local ~/.tmux.conf.local
+ln -sf $(pwd)/.gitconfig ~/.gitconfig
 
