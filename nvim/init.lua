6379524394f9de4570649b0aa41dbf4dec343ff4
Bongjun <bongjun.jang@kaist.ac.kr>
2024-06-26 23:48:43 +0900

    setup nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+require("config.lazy")
+
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+
+vim.opt.wrap = true
+vim.opt.breakindent = true
