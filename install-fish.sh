ea55ccdc955354a0ddf3970fe6a2769bca828b50
Bongjun <bongjun.jang@kaist.ac.kr>
2024-06-27 11:37:05 +0900

    update install script

--- install-fish.sh
+++ install-fish.sh
@@ -1,4 +1,4 @@
-curl https://github.com/fish-shell/fish-shell/releases/download/3.7.1/fish-3.7.1.tar.xz
+curl -LO https://github.com/fish-shell/fish-shell/releases/download/3.7.1/fish-3.7.1.tar.xz
 tar -xvf fish-3.7.1.tar.xz
 cd fish-3.7.1
 cmake .
