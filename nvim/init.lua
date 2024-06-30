f7f66b6c7bb3c7420148e360b50f9cdc2c2f73bf
Bongjun <bongjun.jang@kaist.ac.kr>
2024-06-30 06:04:04 +0000

    install LazyVim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,23 +1,2 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-local lspconfig = require('lspconfig')
-lspconfig.ocamllsp.setup{}
-lspconfig.pyright.setup{}
-
-require('tokyonight').setup({
-  transparent = true,
-    styles = {
-      comments = { italic = true },
-    }
-})
-
-vim.opt.number = true
-vim.opt.mouse = 'a'
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = false
-
-vim.opt.wrap = true
-vim.opt.breakindent = true
-
-vim.cmd [[colorscheme tokyonight]]
