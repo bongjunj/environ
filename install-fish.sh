1441ec7e9cb6779f581393910b41f6e06a45a675
Bongjun <bongjun.jang@kaist.ac.kr>
2024-07-26 05:40:05 +0000

    update scripts

--- install-fish.sh
+++ install-fish.sh
@@ -1,3 +1,5 @@
+set -e
+
 curl -LO https://github.com/fish-shell/fish-shell/releases/download/3.7.1/fish-3.7.1.tar.xz
 tar -xvf fish-3.7.1.tar.xz
 cd fish-3.7.1
---
+++ install-pyenv.sh
@@ -0,0 +1,5 @@
+set -
+
+curl https://pyenv.run | bash
+pyenv install 3.12.4
+pyenv global 3.12.4
