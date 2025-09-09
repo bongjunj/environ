537c33e2a709adf51064980143d5793c938616a0
Bongjun <bongjun.jang@kaist.ac.kr>
2025-09-09 19:20:09 +0900

    [Fish] update

--- config.fish
+++ config.fish
@@ -3,25 +3,27 @@ if status is-interactive
 end
 
 # opam configuration
-source /home/bonjune/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 
+# PYTHON
 set -Ux PYENV_ROOT $HOME/.pyenv
-set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+fish_add_path -p $PYENV_ROOT/bin
+pyenv init - | source
 
+# RUBY
 set -Ux RBENV_ROOT $HOME/.rbenv
 fish_add_path $RBENV_ROOT/bin
 fish_add_path $HOME/.rbenv/plugins/ruby-build/bin
 status --is-interactive; and source (rbenv init -|psub)
 
+# NEOVIM
 fish_add_path /opt/nvim-linux-x86_64/bin
 
-# texlive
+# TEXLIVE
 set -Ux TEXLIVE_ROOT /usr/local/texlive/2025
 fish_add_path $TEXLIVE_ROOT/bin/x86_64-linux
 
-pyenv init - | source
-
-# Lean Theorem Prover
+# Lean
 if test -d '$HOME/.elan/bin/'
   fish_add_path $HOME/.elan/bin/
 end
