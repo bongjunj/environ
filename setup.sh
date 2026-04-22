ca65004c165d384b755b938f2692bf89e24518a1
Bongjun <bongjun.jang@kaist.ac.kr>
2026-04-22 10:52:10 +0000

    update tmux conf

--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@ mkdir -p ~/.config/nvim
 
 ln -sf $(pwd)/config.fish ~/.config/fish/config.fish
 ln -sf $(pwd)/.tmux.conf ~/.tmux.conf
-ln -sf $(pwd)/.tmux.conf.local ~/.tmux.conf.local
 ln -sf $(pwd)/.gitconfig ~/.gitconfig
 ln -sf $(pwd)/init.lua ~/.config/nvim/init.lua
 
-cp ./fish_prompt.fish ~/.config/fish/functions/
