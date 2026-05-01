e1f0c4adfb97ae1996d2179e94e4848b9845c803
Bongjun <bongjun.jang@kaist.ac.kr>
2026-05-01 06:06:17 +0000

    diagnostics

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -9,6 +9,8 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clip
 vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line to system clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]], { desc = "Paste from system clipboard" })
 
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
+
 nmap("gd", vim.lsp.buf.definition, "Go to Definition")
 nmap("gD", vim.lsp.buf.declaration, "Go to Declaration")
 nmap("gr", vim.lsp.buf.references, "Find References")
