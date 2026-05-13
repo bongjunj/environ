80c7b7a618fcb8a1a648b5cc0df048be9a03b053
Bongjun <bongjun.jang@kaist.ac.kr>
2026-05-13 06:56:45 +0000

    easy tmux/neovim nagivation keys

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -21,6 +21,15 @@ return {
     end,
   },
   {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+      { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Navigate left" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Navigate down" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Navigate up" },
+      { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Navigate right" },
+    },
+  },
+  {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
