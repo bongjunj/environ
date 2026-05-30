c8d44a9606cbb870c30fd97e0c811108471adab0
Bongjun <bongjun.jang@kaist.ac.kr>
2026-05-30 10:47:09 +0000

    fix zj

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 mkdir -p ~/.config/fish
 mkdir -p ~/.config/helix
+mkdir -p ~/.config/zellij
+
 if [ -e ~/.config/nvim ] && [ ! -L ~/.config/nvim ]; then
   rm -rf ~/.config/nvim
 fi
 
 ln -sf "$(pwd)/config.fish" ~/.config/fish/config.fish
 ln -sf "$(pwd)/helix/config.toml" ~/.config/helix/config.toml
-ln -sf "$(pwd)/zellij/config.toml" ~/.config/zellij/config.toml
+ln -sf "$(pwd)/zellij/config.kdl" ~/.config/zellij/config.kdl
 ln -sf "$(pwd)/.tmux.conf" ~/.tmux.conf
 ln -sf "$(pwd)/.gitconfig" ~/.gitconfig
 ln -sfn "$(pwd)/nvim" ~/.config/nvim
