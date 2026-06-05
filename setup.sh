52bafa99c76c9b1a2bb6896d6771777effad487b
Bongjun <bongjun.jang@kaist.ac.kr>
2026-06-05 17:49:59 +0900

    update helix for latex

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ fi
 
 ln -sf "$(pwd)/config.fish" ~/.config/fish/config.fish
 ln -sf "$(pwd)/helix/config.toml" ~/.config/helix/config.toml
+ln -sf "$(pwd)/helix/languages.toml" ~/.config/helix/languages.toml
 ln -sf "$(pwd)/zellij/config.kdl" ~/.config/zellij/config.kdl
 ln -sf "$(pwd)/.tmux.conf" ~/.tmux.conf
 ln -sf "$(pwd)/.gitconfig" ~/.gitconfig
